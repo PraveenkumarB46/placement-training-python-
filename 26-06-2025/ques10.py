af0bfc0c06ef0456ab50a48c35661ee58e6151c4
PraveenkumarB46 <151357767+PraveenkumarB46@users.noreply.github.com>
2025-06-26 19:44:00 +0530

    Create ques10.py

---
+++ 26-06-2025/ques10.py
@@ -0,0 +1,12 @@
+def merge_sorted_lists(a, b):
+    result = []
+    i = j = 0
+    while i < len(a) and j < len(b):
+        if a[i] < b[j]:
+            result.append(a[i])
+            i += 1
+        else:
+            result.append(b[j])
+            j += 1
+    result += a[i:] + b[j:]
+    return result
