9f860f727594de2c4bc70bb62570a627917e0c30
PraveenkumarB46 <151357767+PraveenkumarB46@users.noreply.github.com>
2025-06-28 19:28:21 +0530

    Create ques5.py

---
+++ 28-06-2025/ques5.py
@@ -0,0 +1,5 @@
+//Movie Recommendation System
+preferences = {'Action': ['The Dark Knight', 'Inception'], 'Comedy': ['The Hangover', 'Superbad']}
+user_preference = 'Action'
+recommendations = preferences.get(user_preference, [])
+print("Recommendations:", recommendations)
